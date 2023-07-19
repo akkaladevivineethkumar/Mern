7725fb5cc6c50997e8d9f4750466b35ace8d29c6
akkaladevivineethkumar <akkaladevivineethkumar1998@gmail.com>
2023-07-19 14:14:56 +0000

    nodejs_class1

---
+++ calculator.js
@@ -0,0 +1,10 @@
+// export let add = (a, b) => {
+//   return a + b;
+// };
+// export let sub = (a, b) => {
+//   return a - b;
+// };
+let a = 1
+module.exports = 5*2
+// // exports.sub = sub;
+
---
+++ import.js
@@ -0,0 +1,4 @@
+// import {add, sub} from './calculator.mjs';
+let a = require("./calculator");
+console.log(a);
+// console.log(sub(5, 6));
